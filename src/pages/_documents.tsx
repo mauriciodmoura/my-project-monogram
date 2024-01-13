e25bdd22403a2a4d5bfb8df3543f83030cb1967c
Mauricio De Moura <mauriciodemoura@Mauricios-Mac-mini.local>
2024-01-13 15:36:28 -0300

    feat: vercel analytics and speedinsights

--- src/pages/_documents.tsx
+++ src/pages/_documents.tsx
@@ -1,6 +1,4 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Document() {
   return (
@@ -9,8 +7,6 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <Analytics />
-        <SpeedInsights/>
       </body>
     </Html>
   );
